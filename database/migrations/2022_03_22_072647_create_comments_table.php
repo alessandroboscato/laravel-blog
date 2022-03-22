7b150c36126330071fcfef2df334bd3f28c91e52
alebsct <alessandro.boscato@gmail.com>
2022-03-22 19:16:17 +1100

    First commit

---
+++ database/migrations/2022_03_22_072647_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id')->constrained();
+            $table->string('author', 50);
+            $table->string('email', 50);
+            $table->string('content');
+            $table->tinyInteger('approved');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
